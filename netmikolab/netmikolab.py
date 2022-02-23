ee77c18cba7edd80ddb2085bf089c536db875126
Hanon088 <62070088@kmitl.ac.th>
2022-02-23 11:08:04 +0700

    Modified set interface

--- netmikolab/netmikolab.py
+++ netmikolab/netmikolab.py
@@ -1,4 +1,5 @@
 import re
+from turtle import down
 from netmiko import ConnectHandler
 
 def getDataFromDevice(params, command):
@@ -31,23 +32,30 @@ def getIP(params, interface, includeMask=True):
     return int_ip
 
 
-def getAllIPInterface(params):
+def getAllIPInterface(params, includeLoopback=True):
     """Get all ip addresses of a device"""
     command = f"sh ip interface br"
     result = getDataFromDevice(params , command)
-    result = result.split("\n")
-    list_of_ip = []
+    result = result.split("\n")[1:]
+    """list_of_ip = []
     for line in result[1:]:
         line = line.split()
         if line[0].find("Gigabit") != -1:
             line[0] = "G" + line[0][15:]
         list_of_ip.append(line[0:2])
-    """result = []
+    result = []
     for interface_in in list_of_ip:
         if interface == "G" and interface_in[0].find("G") != -1:
             string_out = interface_in[0] + " " + interface_in[1]
-            result.append(string_out)"""
-    return list_of_ip
+            result.append(string_out)
+    return list_of_ip"""
+    ipDict = {}
+    for line in result:
+        line = line.split()[:2]
+        if "Loopback" in line[0] and not includeLoopback:
+            continue
+        ipDict.update({line[0]: line[1]})
+    return ipDict
 
 def getIPinterfaceDes(params, interface):
     command = f"show int description"
@@ -70,9 +78,14 @@ def getIPinterfaceDes(params, interface):
             pass
     return list_of_des
 
-def setipdes_all(device_params):
-    with ConnectHandler(**device_params) as ssh:
-        result =  ssh.send_command("sh cdp nei")
+def setipdes_all(params):
+    result = getDataFromDevice(params, "sh cdp nei")
+    ipDict = getAllIPInterface(params, False)
+    intStatus = {}
+    for intName, intIp in ipDict.items():
+        intName = intName.split("Ethernet")
+        intStatus.update({intName[0][0] + intName[1]: intIp != "unassigned"})
+
     result = result.split("\n")
     result_len = len(result)
     result = result[5:result_len-2]
@@ -87,20 +100,25 @@ def setipdes_all(device_params):
         line.append(result[line_len-2:line_len])
         line[2] = line[2][0][0] + line[2][1]
         output.update({line[1]:"Connected to " + line[2] + " of " + line[0]})
-    with ConnectHandler(**device_params) as ssh:
-        for interface in output:
-            ssh.send_config_set(["Interface " + interface,"des " + output[interface]])
-        ssh.send_config_set(["interface g0/3","des Not Use"])
-        if device_params["ip"] == "172.31.104.6" and interface == "G0/2":
+
+    with ConnectHandler(**params) as ssh:
+        for interface in intStatus:
+            if intStatus[interface]:
+                ssh.send_config_set(["Interface " + interface,"des " + output[interface]])
+            else:
+                ssh.send_config_set(["Interface " + interface,"des Not Use"])
+
+        if params["ip"] == "172.31.104.6" and interface == "G0/2":
             ssh.send_config_set(["interface g0/2","des Connected to Nat"])
         result = ssh.send_command("show int description")
+    #maybe find out what to return
     return output
 
 if __name__ == '__main__':
     device_ip = "172.31.104.4"
     username = "admin"
-    key_file = "rsa2"
-    # key_file="C:\\Users\\Jack\\Documents\\NPA\\rsa2"
+    #key_file = "rsa2"
+    key_file="C:\\Users\\Jack\\Documents\\NPA\\rsa2"
 
     device_params = {"device_type": "cisco_ios",
                     "ip": device_ip,
@@ -114,3 +132,6 @@ if __name__ == '__main__':
     # print(getIP(device_params, "G0/0", False))
     # print(getIP(device_params, "G0/3"))
     # print(getIPinterfaceDes(device_params, "G"))
+    #print(getAllIPInterface(device_params))
+    #print(getAllIPInterface(device_params, False))
+    print(setipdes_all(device_params))
